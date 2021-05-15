37054930671dda970a95a26d0d99705d7e01e5d4
mburakbilgic <muhammed.burak.bilgic@gmail.com>
2021-05-16 01:07:12 +0300

    feat(update_main):main project for hrms

---
+++ hrms/src/main/java/mypackage/hrms/dataAccess/abstracts/JobTitlesDao.java
@@ -0,0 +1,8 @@
+package mypackage.hrms.dataAccess.abstracts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+	
+import mypackage.hrms.entities.concretes.JobTitles;
+public interface JobTitlesDao extends JpaRepository<JobTitles, Integer> {
+
+}
---
+++ hrms/src/test/java/mypackage/hrms/HrmsApplicationTests.java
@@ -0,0 +1,13 @@
+package mypackage.hrms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
