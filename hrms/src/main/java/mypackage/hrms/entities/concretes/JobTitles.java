7eeabf7a70ceb42558aae663555aea0db10db3b5
mburakbilgic <muhammed.burak.bilgic@gmail.com>
2021-07-21 02:54:01 +0300

    fix(lombok):getter and setter updated for lombok

--- hrms/src/main/java/mypackage/hrms/entities/concretes/JobTitles.java
+++ hrms/src/main/java/mypackage/hrms/entities/concretes/JobTitles.java
@@ -3,44 +3,28 @@ package mypackage.hrms.entities.concretes;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "job_titles")
 
 public class JobTitles {
+
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id")
 	private int id;
+
 	@Column(name = "title")
 	private String title;
-
-	JobTitles() {
-
-	}
-
-	public JobTitles(int id, String title) {
-		super();
-		this.id = id;
-		this.title = title;
-	}
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
 
 }
