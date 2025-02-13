a0a0b8d9ddc43294b37b3f1b3f8ee5603c61ac5b
mburakbilgic <53704957+mburakbilgic@users.noreply.github.com>
2025-02-13 23:48:18 +0300

    fix(extends): wrong extends fixed

--- hrms/src/main/java/mypackage/hrms/dataAccess/abstracts/UsersDao.java
+++ hrms/src/main/java/mypackage/hrms/dataAccess/abstracts/UsersDao.java
@@ -2,8 +2,8 @@ package mypackage.hrms.dataAccess.abstracts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import mypackage.hrms.entities.concretes.JobTitles;
+import mypackage.hrms.entities.concretes.Users;
 
-public interface UsersDao extends JpaRepository<JobTitles, Integer> {
+public interface UsersDao extends JpaRepository<Users, Integer> {
 
 }
