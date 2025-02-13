f9d05d07fc08f4acec31b2c468fe6cb3326e605f
mburakbilgic <53704957+mburakbilgic@users.noreply.github.com>
2025-02-13 23:47:24 +0300

    feat(update): springframework latest stable version

--- hrms/src/main/java/mypackage/hrms/HrmsApplication.java
+++ hrms/src/main/java/mypackage/hrms/HrmsApplication.java
@@ -2,30 +2,24 @@ package mypackage.hrms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 
 @SpringBootApplication
-@EnableSwagger2
+@OpenAPIDefinition(
+		info = @Info(
+				title = "HRMS API",
+				version = "1.0",
+				description = "HRMS API Dokümantasyonu"
+		)
+)
 public class HrmsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HrmsApplication.class, args);
 	}
-	
-	@Bean
-    public Docket api() { 
-        return new Docket(DocumentationType.SWAGGER_2)  
-          .select()                                  
-          .apis(RequestHandlerSelectors.basePackage("mypackage.hrms"))                    
-          .build();                                           
-    }
-	
+
 	// TODO update that at night of 7/21 and followed 7/22
-	// mernis and system vertification needed, under the core create the utilities and command the all update-delete-verifiy operations
+	// mernis and system verification needed, under the core create the utilities
+	// and command all update-delete-verify operations
 }
