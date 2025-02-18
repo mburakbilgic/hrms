7245c158f076e01702c97e6641502ce8a1004e3c
mburakbilgic <53704957+mburakbilgic@users.noreply.github.com>
2025-02-18 13:54:40 +0300

    refactor(beautify): updated for clean read

--- src/main/java/mypackage/hrms/dataAccess/abstracts/JobTitlesDao.java
+++ src/main/java/mypackage/hrms/dataAccess/abstracts/JobTitlesDao.java
@@ -1,7 +1,7 @@
 package mypackage.hrms.dataAccess.abstracts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-	
+
 import mypackage.hrms.entities.concretes.JobTitles;
 public interface JobTitlesDao extends JpaRepository<JobTitles, Integer> {
 
