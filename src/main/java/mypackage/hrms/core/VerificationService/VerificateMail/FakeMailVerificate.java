ebe9d9b4c6c8f220481f176d1255be7b8925623e
mburakbilgic <53704957+mburakbilgic@users.noreply.github.com>
2025-03-03 04:40:58 +0300

    feat(EmailService): fake controller

--- src/main/java/mypackage/hrms/core/VerificationService/VerificateMail/FakeMailVerificate.java
+++ src/main/java/mypackage/hrms/core/VerificationService/VerificateMail/FakeMailVerificate.java
@@ -1,13 +1,27 @@
 package mypackage.hrms.core.VerificationService.VerificateMail;
 
 import org.springframework.stereotype.Service;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class FakeMailVerificate implements MailVerificateService{
 
+	private final Map<String, String> verificationCodes = new HashMap<>();
+
 	@Override
-	public boolean isEmail(String email) {
+	public boolean sendVerificationEmail(String email, String code) {
+		verificationCodes.put(email, code);
+		System.out.println("Fake email sent to: " + email + " with code: " + code);
 		return true;
 	}
 
+	@Override
+	public boolean verifyEmail(String email, String code) {
+		if (verificationCodes.containsKey(email) && verificationCodes.get(email).equals(code)) {
+			verificationCodes.remove(email);
+			return true;
+		}
+		return false;
+	}
 }
