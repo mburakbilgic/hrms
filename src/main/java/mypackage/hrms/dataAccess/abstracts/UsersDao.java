65734291158e0fc9f15f89cdf9b23f5f6791a457
mburakbilgic <53704957+mburakbilgic@users.noreply.github.com>
2025-03-03 04:39:41 +0300

    feat(Users): email duplicate controller

--- src/main/java/mypackage/hrms/dataAccess/abstracts/UsersDao.java
+++ src/main/java/mypackage/hrms/dataAccess/abstracts/UsersDao.java
@@ -3,7 +3,8 @@ package mypackage.hrms.dataAccess.abstracts;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import mypackage.hrms.entities.concretes.Users;
+import java.util.Optional;
 
 public interface UsersDao extends JpaRepository<Users, Integer> {
-
+    Optional<Users> findByEmail(String email);
 }
