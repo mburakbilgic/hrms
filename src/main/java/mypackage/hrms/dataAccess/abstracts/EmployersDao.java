58c3d47809ec6f456f1d26da6a3106beb27bfac8
mburakbilgic <53704957+mburakbilgic@users.noreply.github.com>
2025-04-19 20:23:28 +0300

    feat(DAO): email search

--- src/main/java/mypackage/hrms/dataAccess/abstracts/EmployersDao.java
+++ src/main/java/mypackage/hrms/dataAccess/abstracts/EmployersDao.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import mypackage.hrms.entities.concretes.Employers;
 
-public interface EmployersDao extends JpaRepository<Employers, Integer> {
+import java.util.Optional;
 
+public interface EmployersDao extends JpaRepository<Employers, Integer> {
+    Optional<Employers> findById(Integer id);
+    Optional<Employers> findByEmail(String email);
 }
