61b258f98ab27c00a45a74fb8c4018e8f7d35d0c
mburakbilgic <53704957+mburakbilgic@users.noreply.github.com>
2025-04-20 17:07:18 +0300

    feat(duplicate-check): findByEmail DAO

--- src/main/java/mypackage/hrms/dataAccess/abstracts/JobTitlesDao.java
+++ src/main/java/mypackage/hrms/dataAccess/abstracts/JobTitlesDao.java
@@ -3,6 +3,9 @@ package mypackage.hrms.dataAccess.abstracts;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import mypackage.hrms.entities.concretes.JobTitles;
-public interface JobTitlesDao extends JpaRepository<JobTitles, Integer> {
 
+import java.util.Optional;
+
+public interface JobTitlesDao extends JpaRepository<JobTitles, Integer> {
+    Optional<JobTitles> findByTitle(String title);
 }
