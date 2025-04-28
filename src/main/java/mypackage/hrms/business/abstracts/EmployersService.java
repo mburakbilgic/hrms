b2634dc5fa0cd9a25d3b38424d6a2d40acae7b0a
mburakbilgic <53704957+mburakbilgic@users.noreply.github.com>
2025-04-28 21:43:56 +0300

    feat/fix(EmployersFields): Required CRUD services for new data fields, and improvements

--- src/main/java/mypackage/hrms/business/abstracts/EmployersService.java
+++ src/main/java/mypackage/hrms/business/abstracts/EmployersService.java
@@ -1,17 +1,18 @@
 package mypackage.hrms.business.abstracts;
 
-import java.util.List;
-
 import mypackage.hrms.core.utilities.notifications.DataNotification;
 import mypackage.hrms.core.utilities.notifications.Notification;
 import mypackage.hrms.entities.concretes.Employers;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
+
 public interface EmployersService {
 	DataNotification<List<Employers>> getAll();
-	Notification add(Employers employers);
-	Notification update(Employers employers);
+	Notification add(Employers employer);
+	Notification update(Employers employer);
 	Notification delete(int id);
 	Notification verifyKYCEmployers(int id, MultipartFile document);
 	Notification verifyEmailEmployers(String email, String code);
+	DataNotification<Employers> findByTaxNumber(String taxNumber);
 }
